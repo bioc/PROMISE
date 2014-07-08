7102ea30464c05ffa13d372497a88cdbe7a6c6d4
Xueyuan Cao Cao <xueyuan.cao@stjude.org>
2014-07-08 21:04:45 +0000

    This update is to provide two options to the PROMISE function. (1) project onto 0 to test association of genomic features with any endpoint variables (proj0 default is FALSE); (2) determine p value by negative binomial (nbperm default is FALSE). The promise.genestat  function is also modified to accept the proj0 option. The examples and document files are also modified to reflect the changes.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PROMISE@92285 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/PROMISE.R
+++ R/PROMISE.R
@@ -1,5 +1,5 @@
 `PROMISE` <-
-function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, seed=13, nperms=10000)
+function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, proj0=FALSE, seed=13, nbperm=FALSE, max.ntail=100, nperms=10000)
 
 {
      # extract data components from objects
@@ -22,7 +22,7 @@ function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, seed=13, nperms
                                           setID=rep(as.character(this.set), length(this.name))))
           }
       }
-
+     #geneset.data<-geneset.data[order(geneset.data[, 1]), ]
      # Match array data and endpttype data by subject identifiers
      array.names<-dimnames(array.data)[[2]]
      endpt.names<-dimnames(endpt.data)[[1]]
@@ -44,6 +44,7 @@ function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, seed=13, nperms
           
      # Subset on array data and phenotype data with matching subject IDs
      Y<-array.data[,array.inc]
+     #Y<-Y[order(dimnames(Y)[[1]]), ]
      X<-endpt.data[endpt.inc,]
      array.names<-array.names[array.inc]
      endpt.names<-endpt.names[endpt.inc]
@@ -82,8 +83,10 @@ function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, seed=13, nperms
      probes<-dimnames(array.data)[[1]]
      nprobes<-length(probes)
      
-     if (!is.null(geneSet)) 
-     {
+     if (nbperm)
+     {      
+       if (!is.null(geneSet)) 
+       {
 
          #Now extract probe and gene set names
          gs.probes<-geneset.data[,1]
@@ -97,7 +100,7 @@ function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, seed=13, nperms
          message(paste("Computing gene-specific statistics:",date()))
          if (is.null(strat.var)) strat<-NULL  
          else  strat<-phtype[,strat.var] 
-         gene.res<-promise.genestat(Y,phtype,ph.pattern, strat)
+         gene.res<-promise.genestat(Y,phtype,ph.pattern, strat, proj0=proj0)
 
          m.nph<-dim(gene.res)
          gene.pvals<-matrix(0,m.nph[1],m.nph[2])
@@ -111,107 +114,322 @@ function(exprSet, geneSet=NULL, promise.pattern, strat.var=NULL, seed=13, nperms
          #set seed for sampling subjects
          message(paste("Set seed: seed = ",seed))
          set.seed(seed)
+         
+         probe.keep<-rep(T,nprobes)
+         probe.done<-rep(F,nprobes)
+         probe.ntail<-matrix(0,m.nph[1],m.nph[2])
+         probe.apt.ntail<-rep(0,m.nph[1])
+         
+         set.keep<-rep(T,ngs.nph[1])
+         set.done<-rep(F,ngs.nph[1])
+         set.ntail<-matrix(0,ngs.nph[1],ngs.nph[2])
+         set.apt.ntail<-rep(0,ngs.nph[1])
+
+         set.probe.ntail<-rep(NA,nprobes)
+         set.probe.keep<-rep(NA,nprobes)
+         set.probe.keep[is.element(probes, geneset.data[, 1])]<-T
+         set.probe.done<-rep(F,nprobes)
+         
+         num.perms<-rep(NA,nprobes)
+         set.perms<-rep(NA,nset)
+         
+         i<-0
+         while((i<nperms)&&(any(probe.keep)))
+         {
+           i<-i+1
+           if (is.null(strat.var)) perm.ind<-sample(nsamp.inc,replace=FALSE)
+           else
+           {
+             strat<-X[,strat.var]
+             ustrat<-sort(unique(strat))
+             nstrat<-length(ustrat)
+             ind<-length(strat)
+             for (j in 1:nstrat)
+             {
+               this.strat<-(strat == ustrat[j])
+               perm.ind[this.strat]<-sample((1:ind)[this.strat],replace=FALSE)
+             }
+           }
+           perm.phtype<-phtype[perm.ind,]
+           
+           if (is.null(strat.var))  perm.strat<-NULL 
+           else  perm.strat<- perm.phtype[,strat.var]
+                    
+           gene.temp<-promise.genestat(Y[probe.keep, ],perm.phtype, ph.pattern, perm.strat, proj0=proj0)
+           set.probe.keep2<-set.probe.keep[!is.na(set.probe.keep)]
+           enrich.temp<-avg.abs.genestat(gene.temp,probes[probe.keep],geneset.data[set.probe.keep2, ])
+           
+           set.ntail[set.keep,]<-set.ntail[set.keep,] + (abs(enrich.temp)>=abs(enrich.res[set.keep, ]))
+           set.apt.ntail[set.keep]<-set.ntail[set.keep, ngs.nph[2]]
+           
+           probe.ntail[probe.keep,]<-probe.ntail[probe.keep,]+(abs(gene.temp)>=abs(gene.res[probe.keep,]))
+           probe.apt.ntail[probe.keep]<- probe.ntail[probe.keep, m.nph[2]]
+           
+           for (j in 1:sum(set.keep)){ 
+             set.probe.ntail[!is.na(set.probe.keep)][is.element(probes[!is.na(set.probe.keep)], geneset.data[set.probe.keep2,][geneset.data[set.probe.keep2, 2]==uset[set.keep][j], 1]) ] <-
+               set.apt.ntail[set.keep][j]  
+           }
+           
+           probe.done[probe.keep]<-(probe.apt.ntail[probe.keep]>=max.ntail & set.probe.ntail[probe.keep] >= max.ntail) |  
+                                   (probe.apt.ntail[probe.keep]>=max.ntail& is.na(set.probe.keep[probe.keep]))  
+           set.probe.done[probe.keep & !is.na(set.probe.keep)]<-probe.done[probe.keep & !is.na(set.probe.keep)]
+           
+           for (j in 1:sum(set.keep)){
+               set.done[set.keep][j] <- set.apt.ntail[set.keep][j]>=max.ntail & 
+                 min(probe.apt.ntail[!is.na(set.probe.keep)][is.element(probes[!is.na(set.probe.keep)], geneset.data[set.probe.keep2,][geneset.data[set.probe.keep2, 2]== uset[set.keep][j], 1])])>=max.ntail
+           }
+           
+           num.perms[probe.done&probe.keep]<-i
+           set.perms[set.done&set.keep]<-i
+           gene.pvals[probe.done&probe.keep, ]<-probe.ntail[probe.done&probe.keep,]/i
+
+           enrich.pvals[set.done&set.keep, ] <-set.ntail[set.done&set.keep, ]/i  
+           probe.keep[probe.done]<-F
+           set.probe.keep[!is.na(set.probe.keep)& set.probe.done]<-F
+           set.keep[set.done]<-F
+         }
+         
+         if (any(probe.keep))
+         {
+           gene.pvals[probe.keep,]<-probe.ntail[probe.keep,]/nperms
+           num.perms[probe.keep]<-nperms
+         }
+                  
+         if (any(set.keep))
+         {
+           enrich.pvals[set.keep,]<-set.ntail[set.keep,]/nperms
+           set.perms[set.keep]<-nperms
+         }
+         dimnames(gene.pvals)[[2]]<-gsub("stat", "perm.p", dimnames(gene.res)[[2]])
+         dimnames(enrich.pvals)[[2]]<-gsub("stat", "perm.p", dimnames(enrich.res)[[2]])
+ 
+         generes.tab<-cbind.data.frame(probeid=probes,
+                                       gene.res,
+                                       gene.pvals,
+                                       nperms=num.perms)
+         
+         enrich.tab<-cbind.data.frame(setid=uset,
+                                      enrich.res,
+                                      enrich.pvals,
+                                      nperms=set.perms)
+         
+         message(paste("Finished at: ",date()))
+         
+         res<-list(generes=generes.tab, setres=enrich.tab)
+       }
+       
+       
+       if (is.null(geneSet)) 
+       {
+         
+         # Now compute results for observed data
+         message(paste("Computing Stats for Observed Data:",date()))
+         message(paste("Computing gene-specific statistics:",date()))
+         if (is.null(strat.var)) strat<-NULL  
+         else  strat<-phtype[,strat.var] 
+         gene.res<-promise.genestat(Y,phtype,ph.pattern, strat, proj0=proj0)
+         
+         m.nph<-dim(gene.res)
+         gene.pvals<-matrix(0,m.nph[1],m.nph[2])
+         
+         #set seed for sampling subjects
+         message(paste("Set seed: seed = ",seed))
+         set.seed(seed)
+         
+         probe.keep<-rep(T,nprobes)
+         probe.done<-rep(F,nprobes)
+         probe.ntail<-matrix(0,m.nph[1],m.nph[2])
+         probe.apt.ntail<-rep(0,m.nph[1])
+         num.perms<-rep(NA,nprobes)
+         
+         i<-0
+         while((i<nperms)&&(any(probe.keep)))
+         {
+           i<-i+1
+           if (is.null(strat.var)) perm.ind<-sample(nsamp.inc,replace=FALSE)
+           else
+           {
+             strat<-X[,strat.var]
+             ustrat<-sort(unique(strat))
+             nstrat<-length(ustrat)
+             ind<-length(strat)
+             for (j in 1:nstrat)
+             {
+               this.strat<-(strat == ustrat[j])
+               perm.ind[this.strat]<-sample((1:ind)[this.strat],replace=FALSE)
+             }
+           }
+           perm.phtype<-phtype[perm.ind,]
+           
+           if (is.null(strat.var))  perm.strat<-NULL 
+           else  perm.strat<- perm.phtype[,strat.var]
+           
+           gene.temp<-promise.genestat(Y[probe.keep, ], perm.phtype, ph.pattern, perm.strat, proj0=proj0)         
+           probe.ntail[probe.keep,]<-probe.ntail[probe.keep,]+(abs(gene.temp)>=abs(gene.res[probe.keep,]))
+           probe.apt.ntail[probe.keep]<- probe.ntail[probe.keep, m.nph[2]]
+           
+           probe.done[probe.keep]<-probe.apt.ntail[probe.keep]==max.ntail         
+           num.perms[probe.done&probe.keep]<-i
+           gene.pvals[probe.done&probe.keep,]<-probe.ntail[probe.done&probe.keep,]/i
+           probe.keep[probe.done]<-F
+         }
+         
+         if (any(probe.keep))
+         {
+           gene.pvals[probe.keep,]<-probe.ntail[probe.keep,]/nperms
+           num.perms[probe.keep]<-nperms
+         }
+         dimnames(gene.pvals)[[2]]<-gsub("stat", "perm.p", dimnames(gene.res)[[2]])
+         
+         generes.tab<-cbind.data.frame(probeid=probes,
+                                       gene.res,
+                                       gene.pvals,
+                                       nperms=num.perms)
+         
+         message(paste("Finished at: ",date()))
+         
+         res<-list(generes=generes.tab, setres=NULL)
+       }
+     } # end of nbperm 
      
+     if (!nbperm)
+     {
+       if (!is.null(geneSet)) 
+       {
+         
+         #Now extract probe and gene set names
+         gs.probes<-geneset.data[,1]
+         gs.sets<-geneset.data[,2]
+         
+         uset<-unique(gs.sets)
+         nset<-length(uset)
+         
+         # Now compute results for observed data
+         message(paste("Computing Stats for Observed Data:",date()))
+         message(paste("Computing gene-specific statistics:",date()))
+         if (is.null(strat.var)) strat<-NULL  
+         else  strat<-phtype[,strat.var] 
+         gene.res<-promise.genestat(Y,phtype,ph.pattern, strat, proj0=proj0)
+         
+         m.nph<-dim(gene.res)
+         gene.pvals<-matrix(0,m.nph[1],m.nph[2])
+         
+         message(paste("Computing gene-set enrichment statistics:",date()))
+         enrich.res<-avg.abs.genestat(gene.res,probes, geneset.data)
+         
+         ngs.nph<-dim(enrich.res)
+         enrich.pvals<-matrix(0,ngs.nph[1],ngs.nph[2])
+         
+         #set seed for sampling subjects
+         message(paste("Set seed: seed = ",seed))
+         set.seed(seed)
+         
          perms<-matrix(NA,nperms,nsamp.inc)
          if (is.null(strat.var)) for (i in 1:nperms) perms[i,]<-sample(nsamp.inc,replace=FALSE)
          else
          {
-            strat<-endpt.data[,strat.var]
-            ustrat<-sort(unique(strat))
-            nstrat<-length(ustrat)
-            ind<-length(strat)
-            for (j in 1:nstrat)
-            {
-               this.strat<-(strat == ustrat[j])
-               for (i in 1:nperms) perms[i,this.strat]<-sample((1:ind)[this.strat],replace=FALSE)
-            }
+           strat<-endpt.data[,strat.var]
+           ustrat<-sort(unique(strat))
+           nstrat<-length(ustrat)
+           ind<-length(strat)
+           for (j in 1:nstrat)
+           {
+             this.strat<-(strat == ustrat[j])
+             for (i in 1:nperms) perms[i,this.strat]<-sample((1:ind)[this.strat],replace=FALSE)
+           }
          }
-     
+         
          message(paste("Computing Stats for Permuted Data: ",date()))    
          for (i in 1:nperms)
          {
-             perm.ind<-unlist(perms[i,])
-             perm.phtype<-phtype[perm.ind,]
-             if (is.null(strat.var))  perm.strat<-NULL 
-                else  perm.strat<- perm.phtype[,strat.var]
-                   
-             gene.temp<-promise.genestat(Y,perm.phtype,ph.pattern, perm.strat)
-             enrich.temp<-avg.abs.genestat(gene.temp,probes,geneset.data)
-
-             enrich.pvals<-enrich.pvals+(abs(enrich.temp)>=abs(enrich.res))
-             gene.pvals<-gene.pvals+(abs(gene.temp)>=abs(gene.res))
+           perm.ind<-unlist(perms[i,])
+           perm.phtype<-phtype[perm.ind,]
+           if (is.null(strat.var))  perm.strat<-NULL 
+           else  perm.strat<- perm.phtype[,strat.var]
+           
+           gene.temp<-promise.genestat(Y,perm.phtype,ph.pattern, perm.strat, proj0=proj0)
+           enrich.temp<-avg.abs.genestat(gene.temp,probes,geneset.data)
+           
+           enrich.pvals<-enrich.pvals+(abs(enrich.temp)>=abs(enrich.res))
+           gene.pvals<-gene.pvals+(abs(gene.temp)>=abs(gene.res))
          }
-     
+         
          message(paste("Generating Result List: ",date()))
          gene.pvals<-gene.pvals/nperms
          enrich.pvals<-enrich.pvals/nperms
 
+         dimnames(gene.pvals)[[2]]<-gsub("stat", "perm.p", dimnames(gene.res)[[2]])
+         dimnames(enrich.pvals)[[2]]<-gsub("stat", "perm.p", dimnames(enrich.res)[[2]])
+         
          generes.tab<-cbind.data.frame(probeid=probes,
-                                   gene.res,
-                                   perm.p=gene.pvals)
-
+                                       gene.res,
+                                       gene.pvals)
+         
          enrich.tab<-cbind.data.frame(setid=uset,
-                                  enrich.res,
-                                  perm.p=enrich.pvals)
-
+                                      enrich.res,
+                                      enrich.pvals)
+         
          message(paste("Finished at: ",date()))
-     
+         
          res<-list(generes=generes.tab, setres=enrich.tab)
+       }
+       
+       if (is.null(geneSet))
+       {
+         # Now compute results for observed data
+         message(paste("Computing Stats for Observed Data:",date()))
+         message(paste("Computing gene-specific statistics:",date()))
+         if (is.null(strat.var)) strat<-NULL  
+         else  strat<-phtype[,strat.var] 
+         gene.res<-promise.genestat(Y,phtype,ph.pattern, strat, proj0=proj0)
+         
+         m.nph<-dim(gene.res)
+         gene.pvals<-matrix(0,m.nph[1],m.nph[2])
+         
+         #set seed for sampling subjects
+         message(paste("Set seed: seed = ",seed))
+         set.seed(seed)
+         
+         perms<-matrix(NA,nperms,nsamp.inc)
+         if (is.null(strat.var)) for (i in 1:nperms) perms[i,]<-sample(nsamp.inc,replace=FALSE)
+         else
+         {
+           strat<-endpt.data[,strat.var]
+           ustrat<-sort(unique(strat))
+           nstrat<-length(ustrat)
+           ind<-length(strat)
+           for (j in 1:nstrat)
+           {
+             this.strat<-(strat == ustrat[j])
+             for (i in 1:nperms) perms[i,this.strat]<-sample((1:ind)[this.strat],replace=FALSE)
+           }
+         }
+         
+         message(paste("Computing Stats for Permuted Data: ",date()))    
+         for (i in 1:nperms)
+         {
+           perm.ind<-unlist(perms[i,])
+           perm.phtype<-phtype[perm.ind,]
+           if (is.null(strat.var))  perm.strat<-NULL 
+           else  perm.strat<- perm.phtype[,strat.var]
+           
+           gene.temp<-promise.genestat(Y,perm.phtype,ph.pattern, perm.strat, proj0=proj0)
+           gene.pvals<-gene.pvals+(abs(gene.temp)>=abs(gene.res))
+         }
+         
+         message(paste("Generating Result List: ",date()))
+         
+         gene.pvals<-gene.pvals/nperms
+         dimnames(gene.pvals)[[2]]<-gsub("stat", "perm.p", dimnames(gene.res)[[2]])
+         
+         generes.tab<-cbind.data.frame(probeid=probes,
+                                       gene.res,
+                                       gene.pvals)
+         message(paste("Finished at: ",date()))     
+         res<-list(generes=generes.tab, setres=NULL)
+       }       
+       
      }
-     
-     if (is.null(geneSet))
-     {
-        # Now compute results for observed data
-        message(paste("Computing Stats for Observed Data:",date()))
-        message(paste("Computing gene-specific statistics:",date()))
-        if (is.null(strat.var)) strat<-NULL  
-        else  strat<-phtype[,strat.var] 
-        gene.res<-promise.genestat(Y,phtype,ph.pattern, strat)
 
-        m.nph<-dim(gene.res)
-        gene.pvals<-matrix(0,m.nph[1],m.nph[2])
-
-        #set seed for sampling subjects
-        message(paste("Set seed: seed = ",seed))
-        set.seed(seed)
-     
-        perms<-matrix(NA,nperms,nsamp.inc)
-        if (is.null(strat.var)) for (i in 1:nperms) perms[i,]<-sample(nsamp.inc,replace=FALSE)
-        else
-        {
-            strat<-endpt.data[,strat.var]
-            ustrat<-sort(unique(strat))
-            nstrat<-length(ustrat)
-            ind<-length(strat)
-            for (j in 1:nstrat)
-            {
-               this.strat<-(strat == ustrat[j])
-               for (i in 1:nperms) perms[i,this.strat]<-sample((1:ind)[this.strat],replace=FALSE)
-            }
-        }
-     
-        message(paste("Computing Stats for Permuted Data: ",date()))    
-        for (i in 1:nperms)
-        {
-            perm.ind<-unlist(perms[i,])
-            perm.phtype<-phtype[perm.ind,]
-            if (is.null(strat.var))  perm.strat<-NULL 
-               else  perm.strat<- perm.phtype[,strat.var]
-                   
-            gene.temp<-promise.genestat(Y,perm.phtype,ph.pattern, perm.strat)
-            gene.pvals<-gene.pvals+(abs(gene.temp)>=abs(gene.res))
-        }
-     
-        message(paste("Generating Result List: ",date()))
-
-        gene.pvals<-gene.pvals/nperms
-  
-        generes.tab<-cbind.data.frame(probeid=probes,
-                                   gene.res,
-                                   perm.p=gene.pvals)
-        message(paste("Finished at: ",date()))     
-        res<-list(generes=generes.tab, setres=NULL)
-     }
      return(res)
 }
-
--- R/promise.genestat.R
+++ R/promise.genestat.R
@@ -1,5 +1,5 @@
 `promise.genestat` <-
-function(Y,ph.data,ph.pattern, strat=NULL)
+function(Y,ph.data,ph.pattern, strat=NULL, proj0=FALSE)
 
 {
    m<-dim(Y)[1]
@@ -9,21 +9,39 @@ function(Y,ph.data,ph.pattern, strat=NULL)
    ph.stats<-matrix(NA,m,nph)
    #ph.pvals<-matrix(NA,m,nph)
    stat.names<-pval.names<-rep("",nph)
-   for (i in 1:nph)
+   if (!proj0)
    {
-      ph.var<-unlist(strsplit(as.character(ph.pattern$endpt.vars[i]),split=","))
-      temp.ph<-ph.data[,ph.var]
+     for (i in 1:nph)
+     {
+       ph.var<-unlist(strsplit(as.character(ph.pattern$endpt.vars[i]),split=","))
+       temp.ph<-ph.data[,ph.var]
 
-      ph.stat<-as.character(ph.pattern$stat.func[i])
-      ph.wgt<-ph.pattern$stat.coef[i]
-      ph.call<-call(ph.stat,Y,temp.ph, strat)
-      ph.val<-eval(ph.call)
-      ph.stats[,i]<-ph.val
-      stat.names[i]<-paste(unlist(c(unlist(ph.var),"stat")),collapse=".")
-      res<-res+ph.wgt*ph.val
-      sum.wgt<-sum.wgt+abs(ph.wgt)
+       ph.stat<-as.character(ph.pattern$stat.func[i])
+       ph.wgt<-ph.pattern$stat.coef[i]
+       ph.call<-call(ph.stat,Y,temp.ph, strat)
+       ph.val<-eval(ph.call)
+       ph.stats[,i]<-ph.val
+       stat.names[i]<-paste(unlist(c(unlist(ph.var),"stat")),collapse=".")
+       res<-res+ph.wgt*ph.val
+       sum.wgt<-sum.wgt+abs(ph.wgt)
+     }
+     res<-res/sum.wgt
    }
-   res<-res/sum.wgt
+   if (proj0)
+   { 
+     for (i in 1:nph)
+     {
+       ph.var<-unlist(strsplit(as.character(ph.pattern$endpt.vars[i]),split=","))
+       temp.ph<-ph.data[,ph.var]
+
+       ph.stat<-as.character(ph.pattern$stat.func[i])
+       ph.call<-call(ph.stat,Y,temp.ph, strat)
+       ph.val<-eval(ph.call)
+       ph.stats[,i]<-ph.val
+       stat.names[i]<-paste(unlist(c(unlist(ph.var),"stat")),collapse=".")
+       res<-res+ph.val^2
+     }    
+   }  
    colnames(ph.stats)<-stat.names
    stat<-cbind(ph.stats,PROMISE.stat=res)
    return(stat)
